3cdbdd91ff5be8001138aa8af9970322eacb03b7
宮井文 <westtaila42240@gmail.com>
2018-08-28 22:34:19 +0900

    Add alart

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -1,4 +1,11 @@
-$(function() {
-  $
+$(function () {
+  $(".confirm").on('click', function() {
+    if(window.confirm("削除してよろしいですか？")){
+        location.href ="/"
+    }else{
+      window.alert('キャンセルされました');
+      return false;
+    }
+  });
 
 });
